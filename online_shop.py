5b7118ffd98e744b05610ab84e61cb01a262c423
Sunnatxoja Ganiyev <sunnatxojaganiyev@gmail.com>
2025-02-25 09:04:55 +0500

    Update online_shop.py

--- online_shop.py
+++ online_shop.py
@@ -1,23 +1,44 @@
 import json
+from abc import ABC, abstractmethod
 
-
-class Product:
-    def __init__(self, id, name, price):
-        self.id = id
-        self.name = name
-        self.price = price
-
-
+class Product(ABC):
+    @abstractmethod
+    def get_price(self):
+        pass
 
     def __str__(self):
         return f"{self.name} - {self.price} so'm"
 
+class PhysicalProduct(Product):
+    def __init__(self, id, name, price, weight):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    def get_price(self):
+        return self.price
+
+    def __str__(self):
+        return super().__str__() + f" (Og'irlik: {self.weight} kg)"
+
+class DigitalProduct(Product):
+    def __init__(self, id, name, price, file_size):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.file_size = file_size
+
+    def get_price(self):
+        return self.price
+
+    def __str__(self):
+        return super().__str__() + f" (Fayl hajmi: {self.file_size} MB)"
 
 class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-
 
 class Cart:
     def __init__(self):
@@ -27,19 +48,16 @@ class Cart:
         self.items.append(product)
 
     def total(self):
-        return sum(item.price for item in self.items)
-
+        return sum(item.get_price() for item in self.items)
 
 class Order:
     def __init__(self, user, cart):
         self.user = user
         self.cart = cart
 
-
 class Payment:
     def process_payment(self, order):
         print(f"To'lov qabul qilindi: {order.cart.total()} so'm")
-
 
 class Shop:
     def __init__(self):
@@ -47,17 +65,22 @@ class Shop:
 
     def load_products(self, filename):
         with open(filename) as f:
-            self.products = [Product(**item) for item in json.load(f)]
+            products_data = json.load(f)
+            for item in products_data:
+                if item['type'] == 'physical':
+                    product = PhysicalProduct(**item)
+                elif item['type'] == 'digital':
+                    product = DigitalProduct(**item)
+                self.products.append(product)
 
     def show_products(self):
         for product in self.products:
             print(product)
 
-
 def main():
     shop = Shop()
     shop.load_products('products.json')
-
+    
     print("Mahsulotlar ro'yxati:")
     shop.show_products()
 
@@ -78,7 +101,6 @@ def main():
     order = Order(user, cart)
     payment = Payment()
     payment.process_payment(order)
-
 
 if __name__ == "__main__":
     main()
